8dc5414dc518635589e7f6f74091769ae3cb8f77
lizhen <lizhen@zhumengyuan.com>
2017-06-26 19:51:00 +0800

    管理资源 loader

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
+import Data from './data.xml';
   function component() {
     var element = document.createElement('div');
 
-   // Lodash, currently included via a script, is required for this line to work
+   // Lodash，现在由此脚本导入
    // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+       // 将图像添加到我们现有的 div。
+    var myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+
+    console.log(Data);
 
     return element;
   }
