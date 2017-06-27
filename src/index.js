783247664aff6e6d856be91793371113c57010e7
lizhen <lizhen@zhumengyuan.com>
2017-06-27 11:32:53 +0800

    管理输出

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import _ from 'lodash';
-import './style.css';
 import Icon from './icon.png';
 import Data from './data.xml';
   function component() {
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,60 @@
 var path = require('path');
-
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-     module: {
-     rules: [
-       {
-         test: /\.css$/,
-         use: [
-           'style-loader',
-           'css-loader'
-         ]
-       },
-      {
-         test: /\.(png|svg|jpg|gif)$/,
-         use: [
-           'file-loader'
-         ]
-       },
-       {
-         test: /\.(woff|woff2|eot|ttf|otf)$/,
-         use: [
-           'file-loader'
-         ]
-       },
-       {
-         test: /\.(csv|tsv)$/,
-         use: [
-           'csv-loader'
-         ]
-       },
-       {
-         test: /\.xml$/,
-         use: [
-           'xml-loader'
-         ]
-       }
-     ]
-   }
+    entry: './src/index.js',
+    output: {
+        filename: '[hash].[name].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(csv|tsv)$/,
+                use: [
+                    'csv-loader'
+                ]
+            },
+            {
+                test: /\.xml$/,
+                use: [
+                    'xml-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'My App',
+        filename: 'index.html',
+        template: 'src/index.html',
+        inject: true,
+        minify: {
+            removeComments: true,
+            removeScriptTypeAttributes: true,
+            removeAttributeQuotes: true,
+            useShortDoctype: true,
+            decodeEntities: true,
+            collapseWhitespace: true,
+            minifyCSS: true
+        }
+    })]
 };
